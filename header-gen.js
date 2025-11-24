831788e7a65209513b3c24a1e1f9624c28eaaa97
I <tunnelvisionweekly@proton.me>
2025-11-23 23:01:00 -0500

    Added link offset for deeper pages
    Since some pages are nested in folders, they need an offset. The basic idea was implemented, still unsure if it works.

--- header-gen.js
+++ header-gen.js
@@ -1,27 +1,37 @@
-      function loadDoc() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
-            }
-        };
-        xhttp.open("GET", "top-links.xml", true);
-        xhttp.send();
+var foffset = "";
+try {
+    if (offset != "") {
+        console.log("Offset found!");
+        foffset = offset;
     }
+} catch (e) {
+    console.log("No offset set.");
+} 
 
-    function myFunction(xml) {
-        console.log(xml);
-        var i;
-        var xmlDoc = xml.responseXML;
-        var x = xmlDoc.getElementsByTagName("menuItem");
-        var output = "";
-        for (i = 0;i<x.length;i++) {
-            var title = x[i].childNodes[1].childNodes[0].textContent;
-            var link = x[i].childNodes[3].childNodes[0].textContent;
-            // <td><button onclick="window.location.href = 'home.html';">Home</button></td>  
-            output += "<td><button onclick=\"window.location.href = " + link + "\">" + title + "</button></td>"; 
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
         }
-        output += "</tr>";
-        document.getElementById("toptable").innerHTML = output;
-        console.log(output);
+    };
+    xhttp.open("GET", foffset + "top-links.xml", true);
+    xhttp.send();
+}
+
+function myFunction(xml) {
+    console.log(xml);
+    var i;
+    var xmlDoc = xml.responseXML;
+    var x = xmlDoc.getElementsByTagName("menuItem");
+    var output = "";
+    for (i = 0;i<x.length;i++) {
+        var title = x[i].childNodes[1].childNodes[0].textContent;
+        var link = x[i].childNodes[3].childNodes[0].textContent;
+        // <td><button onclick="window.location.href = 'home.html';">Home</button></td>  
+        output += "<td><button onclick=\"window.location.href = " + link + "\">" + title + "</button></td>"; 
     }
+    output += "</tr>";
+    document.getElementById("toptable").innerHTML = output;
+    console.log(output);
+}
