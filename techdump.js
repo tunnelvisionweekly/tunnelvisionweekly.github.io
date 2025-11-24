3e4e72f4e2d778a7d621efc06315f02c350080da
I <tunnelvisionweekly@proton.me>
2025-11-23 21:16:46 -0500

    Attempt to fix XML bug
    In an attempt to fix the XML parsing returning undefined, the parseXML function will now return a list of all parsed XML, for the fillTable function to deal with now.

--- techdump.js
+++ techdump.js
@@ -1,14 +1,14 @@
+var places = ["JEC","DCC","VCC","LOW","MRC","Cogswell","West","Sage","Union"];
+var output = "<h3>Start of Generated Table</h3>";
+
 function fillTable() {
     // var xml = loadDoc("scripts/page-text.xml");
-    var places = ["JEC","DCC","VCC","LOW","MRC","Cogswell","West","Sage","Union"];
-    var output = "<h3>Start of Generated Table</h3>";
-
+    var temp_parsed = loadDoc("page-text.xml"); //,places[i].substring(0,3));
     for (var i = 0;i<places.length;i++) {
-        var temp_parsed = loadDoc("page-text.xml",places[i].substring(0,3));
         output += "<tr><td>";
         // var temp_parsed = parseXML(xml,places[i].substring(0,3));
-        var temp_date = temp_parsed[1];
-        var temp_data = temp_parsed[2];
+        var temp_date = temp_parsed[i][1];
+        var temp_data = temp_parsed[i][2];
         output += places[i] + "</td><td>" + temp_date + "</td><td>" + temp_data + "</td></tr>\n";
         // document.getElementById(places[i]).innerHTML = temp_data;
         // document.getElementById("d" + places[i]).innerHTML = temp_date;
@@ -17,11 +17,11 @@ function fillTable() {
     document.getElementById("infotable").innerHTML = output;
 }
 
-function loadDoc(file,desiredDest) {
+function loadDoc(file) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-        parseXML(this,desiredDest);
+        parseXML(this);
         }
     };
     xhttp.open("GET", file, true);
@@ -32,11 +32,12 @@ function retXML(xml) {
     return xml;
 }
 
-function parseXML(xml,desiredDest) {
-    console.log(xml);
+function parseXML(xml) {
+    // console.log(xml);
     var i;
     var xmlDoc = xml.responseXML;
     var x = xmlDoc.getElementsByTagName("textBlock");
+    var parsedXML = [];
     // var output = "";
     // var bold = false;
     for (i = 0;i<x.length;i++) {
@@ -44,15 +45,17 @@ function parseXML(xml,desiredDest) {
         var date = x[i].childNodes[3].childNodes[0].textContent;
         var data = x[i].childNodes[5].childNodes[0].textContent;
         // if (dest == "JEC") {bold = true; output += "<b>";}
-        if (dest == desiredDest) {
-            return [dest,date,data];
-        }
+        // if (dest == desiredDest) {
+        parsedXML.push([dest,date,data]);
+        // }
         // x[0].childNodes[1].childNodes[0] <== change the x[0] to change the textBlock, the childNodes[1] to either 1,3, or 5 for each block. Keep the last one the same.
         // output += "msg " + i + ": " + x[i].getElementsByTagName("msg")[0].childNodes[0].nodeValue + "<br>";
         // output += dest + ": (" + date + ") " + data + "<br>";
         // console.log("The child nodes of this msg is: " + x[i].getElementsByTagName("msg")[0].childNodes);
         // if (bold) {output += "</b>";bold = false;}
     }
+    console.log("Parsed the XML!");
+    return parsedXML;
 }
 
 function placeText(desiredDest,text) {
