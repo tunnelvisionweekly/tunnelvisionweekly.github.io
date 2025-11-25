cd0d533fbd2232d48ac89751079d8f9fa8cd3a54
Henry Hanson <henmasterhanson@gmail.com>
2025-11-25 01:03:56 -0800

    Major restructuring complete Only the home page needs updating now.

--- scripts/header-gen.js
+++ scripts/header-gen.js
@@ -8,30 +8,55 @@
 // 	console.log("No offset set.");
 // }
 
-function loadDoc() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			createHeader(this);
-		}
-	};
-	xhttp.open("GET", "/resources/top-links.xml", true);
-	xhttp.send();
-}
+// function loadDoc() {
+// 	var xhttp = new XMLHttpRequest();
+// 	xhttp.onreadystatechange = function () {
+// 		if (this.readyState == 4 && this.status == 200) {
+// 			createHeader(this);
+// 		}
+// 	};
+// 	xhttp.open("GET", "/resources/top-links.xml", true);
+// 	xhttp.send();
+// }
 
-function createHeader(xml) {
-	console.log(xml);
-	var i;
-	var xmlDoc = xml.responseXML;
-	var x = xmlDoc.getElementsByTagName("menuItem");
-	var output = "<tr>";
-	for (i = 0; i < x.length; i++) {
-		var title = x[i].childNodes[1].childNodes[0].textContent;
-		var link = x[i].childNodes[3].childNodes[0].textContent;
-		// <td><button onclick="window.location.href = 'home.html';">Home</button></td>  
-		output += "<td><button onclick=\"window.location.href = " + link + "\">" + title + "</button></td>";
+// function createHeader(xml) {
+// 	console.log(xml);
+// 	var i;
+// 	var xmlDoc = xml.responseXML;
+// 	var x = xmlDoc.getElementsByTagName("menuItem");
+// 	var output = "<tr>";
+// 	for (i = 0; i < x.length; i++) {
+// 		var title = x[i].childNodes[1].childNodes[0].textContent;
+// 		var link = x[i].childNodes[3].childNodes[0].textContent;
+// 		// <td><anchor onclick="window.location.href = 'home.html';">Home</anchor></td>  
+// 		output += "<td><anchor onclick=\"window.location.href = " + link + "\">" + title + "</anchor></td>";
+// 	}
+// 	output += "</tr>";
+// 	document.getElementById("toptable").innerHTML = output;
+// 	console.log(output);
+// }
+
+async function getHeader() {
+	const navlinks = document.getElementById("navlinks");
+	try {
+		const response = await fetch("/resources/header-links.json");
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+		const result = await response.json();
+		for (let link of result) {
+			let anchor = document.createElement("a");
+			if (link["pathname"] == document.location.pathname) {
+				anchor.classList.add("thispage");
+			}
+			anchor.setAttribute("href", link["pathname"]);
+			anchor.appendChild(document.createTextNode(link["title"]));
+			let listitem = document.createElement("li");
+			listitem.appendChild(anchor);
+			navlinks.appendChild(listitem);
+		}
+		console.log(result);
+	} catch (error) {
+		console.error(error.message);
 	}
-	output += "</tr>";
-	document.getElementById("toptable").innerHTML = output;
-	console.log(output);
 }
