9f1387853f6fb20f5668c68b4be8ada4c49e3413
Henry Hanson <henmasterhanson@gmail.com>
2025-12-02 14:51:47 -0500

    Update Issue 10 content

--- scripts/coloring.js
+++ scripts/coloring.js
@@ -1,6 +1,4 @@
 function colorByNumber(element) {
-	console.log(element);
-	console.log("element");
 	var colors = ["transparent", "blue", "red", "green", "orange"];
 	if (element.hasAttribute("style")) {
 		element.removeAttribute("style");
