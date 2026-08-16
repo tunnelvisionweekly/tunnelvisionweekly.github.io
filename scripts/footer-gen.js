a569fd3933104f0d3afe5c955f696bbce1c3e7f7
Henry Hanson <henmasterhanson@gmail.com>
2026-08-16 16:27:33 -0700

    Issue 16 content and footer style update

---
+++ scripts/footer-gen.js
@@ -0,0 +1,34 @@
+(async () => {
+	const navlinks_bottom = document.getElementById("navlinks-bottom");
+	try {
+		const response = await fetch("/resources/footer-links.json");
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+		const result = await response.json();
+		let i = result["issues"].findIndex(issue => issue["pathname"] == document.location.pathname)
+		if (i !== -1) {
+			if (i - 1 >= 0) {
+				let link = result["issues"][i - 1];
+				let anchor = document.createElement("a");
+				anchor.setAttribute("href", link["pathname"]);
+				anchor.appendChild(document.createTextNode("Previous: " + link["title"]));
+				let listitem = document.createElement("li");
+				listitem.appendChild(anchor);
+				navlinks_bottom.appendChild(listitem);
+			}
+			if (i + 1 < result["issues"].length) {
+				let link = result["issues"][i + 1];
+				let anchor = document.createElement("a");
+				anchor.setAttribute("href", link["pathname"]);
+				anchor.appendChild(document.createTextNode("Next: " + link["title"]));
+				let listitem = document.createElement("li");
+				listitem.appendChild(anchor);
+				navlinks_bottom.appendChild(listitem);
+			}
+		}
+		console.log(result);
+	} catch (error) {
+		console.error(error.message);
+	}
+})();
