8914d2c50aac5e7817a34420fed6122d3b88484d
Henry Hanson <henmasterhanson@gmail.com>
2026-08-19 12:43:33 -0700

    Add footer links to oldest/newest issues

--- scripts/footer-gen.js
+++ scripts/footer-gen.js
@@ -7,8 +7,8 @@
 		}
 		const result = await response.json();
 		let i = result["issues"].findIndex(issue => issue["pathname"] == document.location.pathname)
-		if (i !== -1) {
-			if (i - 1 >= 0) {
+		if (i != -1) {
+			if (i > 0) {
 				let link = result["issues"][i - 1];
 				let anchor = document.createElement("a");
 				anchor.setAttribute("href", link["pathname"]);
@@ -17,11 +17,29 @@
 				listitem.appendChild(anchor);
 				navlinks_bottom.appendChild(listitem);
 			}
-			if (i + 1 < result["issues"].length) {
+			if (i < result["issues"].length - 1) {
 				let link = result["issues"][i + 1];
 				let anchor = document.createElement("a");
 				anchor.setAttribute("href", link["pathname"]);
 				anchor.appendChild(document.createTextNode("Next: " + link["title"]));
+				let listitem = document.createElement("li");
+				listitem.appendChild(anchor);
+				navlinks_bottom.appendChild(listitem);
+			}
+			if (i > 1) {
+				let link = result["issues"][0];
+				let anchor = document.createElement("a");
+				anchor.setAttribute("href", link["pathname"]);
+				anchor.appendChild(document.createTextNode("Oldest: " + link["title"]));
+				let listitem = document.createElement("li");
+				listitem.appendChild(anchor);
+				navlinks_bottom.appendChild(listitem);
+			}
+			if (i < result["issues"].length - 2) {
+				let link = result["issues"][result["issues"].length - 1];
+				let anchor = document.createElement("a");
+				anchor.setAttribute("href", link["pathname"]);
+				anchor.appendChild(document.createTextNode("Newest: " + link["title"]));
 				let listitem = document.createElement("li");
 				listitem.appendChild(anchor);
 				navlinks_bottom.appendChild(listitem);
